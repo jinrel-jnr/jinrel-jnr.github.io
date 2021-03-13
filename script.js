630f6beb51e7ca145f4a89ae46ffddf85556d982
Anthony Monteil <cyberteam.contact@gmail.com>
2021-03-13 22:14:05 +0100

    maj html

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("lien JS ok ^^");
+const cardTitle = document.querySelector(".card-header-title");
+const cardContent = document.querySelector(".display");
+let bool = false;
+console.log(bool);
+
+cardTitle.addEventListener("click", () => {
+  bool ? false : true;
+
+  if (bool) {
+    cardTitle.style.color = "white";
+    cardTitle.style.backgroundColor = "hsl(217, 71%, 45%)";
+
+    cardContent.classList = "blockOpen";
+    bool = false;
+  } else {
+    cardContent.classList = "blockClose";
+    bool = true;
+  }
+});

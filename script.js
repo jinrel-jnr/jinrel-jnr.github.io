9f31f7fa2bc7a1a7966747719efc0f7c2164a3eb
Anthony Monteil <cyberteam.contact@gmail.com>
2021-03-13 23:03:22 +0100

    maj html

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const cardTitle = document.querySelector(".card-header-title");
-const cardContent = document.querySelector(".display");
-let bool = false;
-console.log(bool);
+const cardTitle2 = document.getElementById("card2");
+const cardTitle3 = document.getElementById("card3");
+const cardContent = document.getElementById("display1");
+const cardContent2 = document.getElementById("display2");
+const cardContent3 = document.getElementById("display3");
 
 cardTitle.addEventListener("click", () => {
-  bool ? false : true;
+  console.log("ok ok ok");
+  cardContent.classList.toggle("blockOpen");
+});
 
-  if (bool) {
-    cardTitle.style.color = "white";
-    cardTitle.style.backgroundColor = "hsl(217, 71%, 45%)";
+cardTitle2.addEventListener("click", () => {
+  console.log("ok ok ok");
+  cardContent2.classList.toggle("blockOpen");
+});
 
-    cardContent.classList = "blockOpen";
-    bool = false;
-  } else {
-    cardContent.classList = "blockClose";
-    bool = true;
-  }
+cardTitle3.addEventListener("click", () => {
+  console.log("ok ok ok");
+  cardContent3.classList.toggle("blockOpen");
 });

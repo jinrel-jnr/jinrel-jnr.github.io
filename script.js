e4fd7a5d1d4f70baf388bba8fb684be695f2318e
Anthony Monteil <cyberteam.contact@gmail.com>
2021-03-14 10:22:22 +0100

    Correction de l'affichage des cards - maj mineure du contenu

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const cardTitle = document.querySelector(".card-header-title");
-const cardTitle2 = document.getElementById("card2");
-const cardTitle3 = document.getElementById("card3");
-const cardContent = document.getElementById("display1");
-const cardContent2 = document.getElementById("display2");
-const cardContent3 = document.getElementById("display3");
+const cardTitleUnique = document.querySelectorAll(".card-header-title");
 
-cardTitle.addEventListener("click", () => {
-  console.log("ok ok ok");
-  cardContent.classList.toggle("blockOpen");
-});
+cardTitleUnique.forEach((title) => {
+  console.log(title);
+  title.addEventListener("click", (e) => {
+    switch (e.target.id) {
+      case "1":
+        document.querySelector("#d1").classList.toggle("blockOpen");
+        break;
+      case "2":
+        document.querySelector("#d2").classList.toggle("blockOpen");
+        break;
+      case "3":
+        document.querySelector("#d3").classList.toggle("blockOpen");
+        break;
 
-cardTitle2.addEventListener("click", () => {
-  console.log("ok ok ok");
-  cardContent2.classList.toggle("blockOpen");
-});
-
-cardTitle3.addEventListener("click", () => {
-  console.log("ok ok ok");
-  cardContent3.classList.toggle("blockOpen");
+      default:
+        null;
+    }
+  });
 });

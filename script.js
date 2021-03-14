4bb24ea14594a44d5b73a9ca264aa96293371f36
Anthony Monteil <cyberteam.contact@gmail.com>
2021-03-14 11:45:11 +0100

    maj projet cours JS en ligne

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const cardTitleUnique = document.querySelectorAll(".card-header-title");
 
 cardTitleUnique.forEach((title) => {
-  console.log(title);
   title.addEventListener("click", (e) => {
     switch (e.target.id) {
       case "1":
+        title.classList.toggle("card-header-title-active");
         document.querySelector("#d1").classList.toggle("blockOpen");
         break;
       case "2":
+        title.classList.toggle("card-header-title-active");
         document.querySelector("#d2").classList.toggle("blockOpen");
         break;
       case "3":
+        title.classList.toggle("card-header-title-active");
         document.querySelector("#d3").classList.toggle("blockOpen");
         break;
 

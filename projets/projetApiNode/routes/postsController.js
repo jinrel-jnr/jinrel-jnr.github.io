fdf7b91cc631cfa0ca9c909d58146accc88c5689
Anthony Monteil <cyberteam.contact@gmail.com>
2021-03-15 20:18:01 +0100

    v1 de l'API

--- projets/projetApiNode/routes/postsController.js
+++ projets/projetApiNode/routes/postsController.js
@@ -45,4 +45,14 @@ router.put("/:id", (req, res) => {
   );
 });
 
+router.delete("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknow : " + req.params.id);
+
+  PostsModel.findByIdAndRemove(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Delete error : " + err);
+  });
+});
+
 module.exports = router;

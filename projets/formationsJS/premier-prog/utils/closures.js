ac7a3baf1dbd91adc1a6fd8746125c0e3176b4a2
Anthony Monteil <cyberteam.contact@gmail.com>
2021-04-05 00:03:56 +0200

    modif closures

--- projets/formationsJS/premier-prog/utils/closures.js
+++ projets/formationsJS/premier-prog/utils/closures.js
@@ -3,7 +3,9 @@ Principe des Closures : fonction dans une fonction qui sauvegarde les variables.
 Incrémentation : 
 monNombre = ++variable   <->  (variable = variable +1) puis  (monNombre = variable)
 monNombre = variable++   <-> (monNombre = variable) puis (variable = variable +1)
+
 */
+// closure = fermeture
 
 let go = () => {
   let unPrix = Number(
@@ -11,69 +13,80 @@ let go = () => {
       "Combien souhaitez vous ajouter à votre panier ?\nIndiquez un montant :"
     )
   );
+  console.log("Montant de votre article : " + unPrix + "€.");
   return unPrix;
 };
 
-// Déclaration variable totalArticles en dehors de la foncion... :-/
+function start() {
+  // Déclaration variable totalArticles en dehors de la foncion... :-/
 
-let totalArticles = 0;
-let achat = 0;
-
-function panierOld() {
-  achat = go();
-  totalArticles += achat;
-  return totalArticles, achat;
-}
-let resultatPanier = 0;
-console.log(
-  "Première version avec la déclaration hors de la fonction :\n" +
-    "Encours panier : " +
-    totalArticles +
-    "€ + " +
-    panierOld() +
-    totalArticles +
-    "€ achat. \nMontant total : " +
-    (totalArticles + panierOld())
-);
-console.log(
-  "Première version avec la déclaration hors de la fonction :\nEncours panier : " +
-    totalArticles +
-    "€ + " +
-    panierOld() +
-    "€ achat."
-);
-
-// Sans la closure : en voulant déclarer dans la fonction, se réinialise à chaque fois
-function panierBis(achat) {
   let totalArticles = 0;
-  totalArticles += achat;
-  return totalArticles;
-}
 
-console.log(
-  "Version avec la déclaration dans la fonction :\n" +
-    panierBis(50) +
-    "\n" +
-    panierBis(60) +
-    "\n"
-);
+  function panierOld() {
+    console.log("Première version avec la déclaration hors de la fonction :\n");
+    totalArticles += go();
+    return totalArticles;
+  }
+  console.log("Encours panier : " + panierOld() + "€");
+  console.log("Encours panier : " + panierOld() + "€");
 
-// Avec la closure : variable locale accessible en dehors de la fonction
-function monPanier() {
-  let totalArticles = 0;
-  let achat = go();
-  let closurePanier = () => (totalArticles += achat);
-  return closurePanier;
-}
+  // Sans la closure : en voulant déclarer dans la fonction, se réinialise à chaque fois
+  function panierBis() {
+    console.log(
+      "Version avec la déclaration dans la fonction :\n Le montant se réinitialise à chaque appel de la fonction."
+    );
+    let totalArticles = 0;
+    totalArticles += go();
+    return totalArticles;
+  }
 
-let panier = monPanier();
-console.log(panier());
-console.log(panier());
-console.log(panier());
-
-function constructionMessageLog(uneFonction) {
   console.log(
-    "Première version avec la déclaration hors de la fonction :\nEncours panier : " +
-      uneFonction
+    "Valeur1 : " +
+      panierBis() +
+      "€\n" +
+      "ValeurRemplacé2 : " +
+      panierBis() +
+      "€\n"
   );
+
+  // Avec la closure : variable locale accessible en dehors de la fonction
+  function monPanier() {
+    let totalArticles = 0;
+    let achat = 0;
+    let closureAchat = () => (achat = go());
+    let closurePanier = () => (totalArticles += closureAchat());
+    return closurePanier;
+  }
+
+  let panier = monPanier();
+
+  console.log("Montant de votre panier : " + panier() + "€.");
+  console.log("Montant de votre panier : " + panier() + "€.");
+  console.log("Montant de votre panier : " + panier() + "€.");
+
+  function constructionMessageLog(uneFonction) {
+    console.log(
+      "Première version avec la déclaration hors de la fonction :\nEncours panier : " +
+        uneFonction
+    );
+  }
+}
+
+function startTimer() {
+  function timer() {
+    let max = Number(prompt("Quelle limite souhaitez-vous pour le timer ?"));
+    let secondes = 0;
+    let closure = () => {
+      return ++secondes;
+    };
+
+    return closure;
+  }
+
+  let monTimer = timer();
+  console.log(monTimer());
+  console.log(monTimer());
+  console.log(monTimer());
+
+  timer();
 }
